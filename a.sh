229612bc5c89c635bf1b5c73b4cce18c71391911
Your Name <you@example.com>
2016-07-30 17:01:59 +0200

    test

--- a.sh
+++ a.sh
@@ -1,1 +1,43 @@
-echo "yes"
+#
+# Write colored output
+#
+
+red() {
+  echo -e "\033[0;31m$1\033[0m"
+}
+cyan() {
+  echo -e "\033[0;36m$1\033[0m"
+}
+
+#
+# Abort with an exit message
+#
+
+abort() {
+  echo "`red abort:` $1"
+  # ring
+  echo -en "\007"
+  exit 1
+}
+
+#
+# Ask for sudo access if not already available
+# Note: use `sudo -k` to loose sudo access
+#
+
+check_sudo() {
+  echo "check `cyan sudo`..."
+  # without sudo access return nothing
+  if [[ -z `sudo -n uptime 2>/dev/null` ]]; then
+    # sudo prompt
+    sudo echo >/dev/null
+  fi
+  # one more check if the user abort the password question
+  [[ -z `sudo -n uptime 2>/dev/null` ]] && abort 'sudo required'
+}
+
+#
+# Run this script
+#
+
+check_sudo
---
+++ readme.md
@@ -0,0 +1,3 @@
+```
+wget -qO- https://raw.githubusercontent.com/jeromedecoster/test/master/a.sh | bash
+```
