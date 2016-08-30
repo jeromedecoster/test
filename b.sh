e71ea5346d4dca8f5e7fae4fe262ef7c63d45ae5
Your Name <you@example.com>
2016-08-30 10:07:00 +0200

    again

--- b.sh
+++ b.sh
@@ -1,5 +1,12 @@
 rm -fr /tmp/dots
 mkdir -p /tmp/dots
 cd /tmp/dots
-wget -qO- https://github.com/jeromedecoster/test/archive/master.tar.gz | tar xvz --strip 1
+wget -qO- https://github.com/jeromedecoster/test/archive/master.tar.gz | tar xz --strip 1
+
+sudo rm -fr /usr/local/lib/dots
 sudo cp -R /tmp/dots /usr/local/lib/dots
+
+ls -1 /usr/local/lib/dots/bin | while read file; do
+  rm -f /usr/local/bin/$file
+  ln -s /usr/local/lib/dots/bin/$file /usr/local/bin/$file
+done
